e2f53bbddb42ad2de700805a71400c750c8401a2
KOJEOMSTUDIO\kojeo <kojeomstudio@gmail.com>
2026-01-04 00:55:26 +0900

    feat : 배치 스크립트 수정.

--- webui-user-no-gpu.bat
+++ webui-user-no-gpu.bat
@@ -1,11 +1,11 @@
 @echo off
 
 REM C:\Users\[username]\AppData\Local\Programs\Python\Python310\python.exe
-set PYTHON=
+set PYTHON=C:\Users\kojeo\AppData\Local\Programs\Python\Python310\python.exe
 set GIT=
 set VENV_DIR=
 
 REM --skip-torch-cuda-test (cuda 지원 GPU가 아닌 경우 사용)
-set COMMANDLINE_ARGS=
+set COMMANDLINE_ARGS=--skip-torch-cuda-test
 
 call webui.bat
