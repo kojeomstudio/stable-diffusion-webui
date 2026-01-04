17ab8da59b4ea38f296baa6c8748b042799f82f6
KOJEOMSTUDIO\kojeo <kojeomstudio@gmail.com>
2026-01-04 22:26:33 +0900

    misc : no-gpu 옵션 수정 (.bat)

--- webui-user-no-gpu.bat
+++ webui-user-no-gpu.bat
@@ -5,7 +5,8 @@ set PYTHON=C:\Users\kojeo\AppData\Local\Programs\Python\Python310\python.exe
 set GIT=
 set VENV_DIR=
 
-REM --skip-torch-cuda-test (cuda 지원 GPU가 아닌 경우 사용)
-set COMMANDLINE_ARGS=--skip-torch-cuda-test
+REM cpu로만 이미지 생성하려면 아래 옵션을 전부 사용한다.
+REM https://github.com/AUTOMATIC1111/stable-diffusion-webui/wiki/Command-Line-Arguments-and-Settings
+set COMMANDLINE_ARGS=--use-cpu all --precision full --no-half --skip-torch-cuda-test
 
 call webui.bat
